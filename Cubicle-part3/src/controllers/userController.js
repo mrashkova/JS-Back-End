2f5880451e877b0641938871210864c86cfd14e9
mrashkova <mrashkkova@gmail.com>
2023-10-06 14:21:26 +0300

    repeat password validation, register user and save it to the DB

--- Cubicle-part3/src/controllers/userController.js
+++ Cubicle-part3/src/controllers/userController.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
+const userService = require("../services/userService");
 
 router.get("/register", (req, res) => {
   res.render("user/register");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   console.log(req.body);
   const { username, password, repeatPassword } = req.body;
 
+  await userService.register({ username, password, repeatPassword });
   res.redirect("/users/login");
 });
 
--- Cubicle-part3/src/services/userService.js
+++ Cubicle-part3/src/services/userService.js
@@ -1,1 +1,9 @@
-exports.register = (username, password, repeatPassword) => {};
+const User = require("../models/User");
+
+// 3 - Layer architecture
+// Controller -> Service -> Domain Model
+// if (rpass === pass)
+
+exports.register = (userData) => {
+  return User.create(userData);
+};
